a7f75578c35a0defe0e95f64561e8b395987a562
David MTZD <davidmtzd21512@gmail.com>
2025-02-18 16:39:25 -0600

    se crearon los metodos nesesarios para la funcionalidad de sumar, restar, dividir, y multiplicar

---
+++ src/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+    double resultado;
+
+    public double sumar(double valor1, double valor2) {
+        return resultado = valor1 + valor2;
+    }
+
+    public double restar(double valor1, double valor2) {
+        return resultado = valor1 - valor2;
+    }
+
+    public double dividir(double valor1, double valor2) {
+        return resultado = valor1 / valor2;
+    }
+
+    public double multiplicar(double valor1, double valor2) {
+        return resultado = valor1 * valor2;
+    }
+}
